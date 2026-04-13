73ed080fb114d1ee8a941a48b04203a8f2010612
TXMMYBXY <altusha_03@bk.ru>
2026-04-13 15:59:08 +0700

    addded migrations, seeder, fixed some bugs

--- DocumentFlowServer.Infrastructure/Data/ApplicationDbContextFactory.cs
+++ DocumentFlowServer.Infrastructure/Data/ApplicationDbContextFactory.cs
@@ -4,26 +4,27 @@ using Microsoft.Extensions.Configuration;
 
 namespace DocumentFlowServer.Infrastructure.Data;
 
-public class ApplicationDbContextFactory 
-    : IDesignTimeDbContextFactory<ApplicationDbContext>
-{
-    public ApplicationDbContext CreateDbContext(string[] args)
-    {
-        var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
-
-        var configuration = new ConfigurationBuilder()
-            .SetBasePath(Directory.GetCurrentDirectory())
-            .AddJsonFile("appsettings.json")
-            .Build();
-
-        var connectionString = configuration.GetConnectionString("DefaultConnection");
-
-        optionsBuilder
-            .UseLazyLoadingProxies()
-            .UseMySql(
-                connectionString,
-                new MySqlServerVersion(new Version(8, 0, 21)));
-
-        return new ApplicationDbContext(optionsBuilder.Options);
-    }
-}
+// public class ApplicationDbContextFactory : IDesignTimeDbContextFactory<ApplicationDbContext>
+// {
+//     public ApplicationDbContext CreateDbContext(string[] args)
+//     {
+//         var optionsBuilder = new DbContextOptionsBuilder<ApplicationDbContext>();
+//
+//         var configuration = new ConfigurationBuilder()
+//             .SetBasePath(Directory.GetCurrentDirectory())
+//             .AddJsonFile("Configuration/appsettings.json")
+//             .Build();
+//
+//         var connectionString = configuration.GetConnectionString("DefaultConnection");
+//         Console.WriteLine($"Connection: {connectionString}");
+//         ArgumentException.ThrowIfNullOrWhiteSpace(connectionString);
+//         
+//         optionsBuilder
+//             .UseLazyLoadingProxies()
+//             .UseMySql(
+//                 connectionString,
+//                 new MySqlServerVersion(new Version(8, 0, 21)));
+//
+//         return new ApplicationDbContext(optionsBuilder.Options);
+//     }
+// }
