0c3d76b19d71cb5846350903117987e6f7c9ad3c
TXMMYBXY <altusha_03@bk.ru>
2026-06-02 20:36:09 +0700

    deleted bug-migrations, and update contrainers settings

--- DocumentFlowServer.Worker/Configuration/WorkerSettings.cs
+++ DocumentFlowServer.Worker/Configuration/WorkerSettings.cs
@@ -2,7 +2,7 @@ namespace DocumentFlowServer.Worker.Configuration;
 
 public class WorkerSettings
 {
-    public string ApiBaseUrl { get; set; } = "http://localhost:5189/api";
+    public string ApiBaseUrl { get; set; } = "http://api:80/api";
     public TimeSpan PollInterval { get; set; } = TimeSpan.FromSeconds(10);
     public int MaxRetries { get; set; } = 3;
     public string ApiKey { get; set; } = "secretkeyforworker";
--- DocumentFlowServer.Worker/Program.cs
+++ DocumentFlowServer.Worker/Program.cs
@@ -3,6 +3,12 @@ using Microsoft.Extensions.Hosting;
 
 var builder = Host.CreateApplicationBuilder(args);
 
+var workerApiKey = Environment.GetEnvironmentVariable("WORKER_API_KEY");
+if (!string.IsNullOrWhiteSpace(workerApiKey) && builder.Environment.IsProduction())
+{
+    builder.Configuration["WorkerSettings:ApiKey"] = workerApiKey;
+}
+
 var startup = new Startup(builder.Configuration);
 startup.ConfigureServices(builder.Services);
 
